d562eb1ccf8fded5423d3e81d158a1871bea4552
Scott Sikorski <sgs.sikorski20@gmail.com>
2024-12-03 19:51:39 -0500

    Update to fill-mask task

--- generateSentiment.py
+++ generateSentiment.py
@@ -1,6 +1,6 @@
 from transformers import (
     AutoTokenizer,
-    AutoModelForSequenceClassification,
+    AutoModelForMaskedLM,
     pipeline,
 )
 from datasets import load_dataset
@@ -18,13 +18,12 @@ _tokenizer = AutoTokenizer.from_pretrained(
     **model_args,
 )
 
-_model = AutoModelForSequenceClassification.from_pretrained(model_name)
+_model = AutoModelForMaskedLM.from_pretrained(model_name)
 
 _classifier = pipeline(
-    "text-classification",
+    "fill-mask",
     model=_model,
     tokenizer=_tokenizer,
-    **model_args,
 )
 
 
@@ -32,7 +31,9 @@ def chunk_text(text, max_length=512, tokenizer=None):
     if tokenizer is None:
         tokenizer = _tokenizer
     tokens = tokenizer(text, truncation=False)["input_ids"]
-    chunks = [tokens[i : i + max_length] for i in range(0, len(tokens), max_length)]
+    chunks = [
+        tokens[i : i + max_length - 25] for i in range(0, len(tokens), max_length)
+    ]
     return chunks
 
 
@@ -43,13 +44,40 @@ def classify_chunks(chunks, tokenizer=None, classifier=None):
         classifier = _classifier
     scores = []
     for chunk in chunks:
+        chunk += tokenizer(
+            "Of high, medium, or low, the patient's recovery risk is [MASK]",
+            padding=False,
+        )["input_ids"]
         inputs = tokenizer.decode(chunk)
         result = classifier(inputs)
-        scores.append(
-            result[0]["score"]
-            if result[0]["label"] == "LABEL_1"
-            else -result[0]["score"]
-        )
+        label = tokenizer.decode(result[0]["token"])
+        bad = [
+            "high",
+            "likely",
+            "concerning",
+            "increased",
+            "significantly",
+            "notable",
+            "limited",
+        ]
+        good = ["low", "negative", "0", "not"]
+        neutral = [
+            "medium",
+            "unclear",
+            "?",
+            "neutral",
+            "unknown",
+            "normal",
+            "consistent",
+        ]
+        if label in bad:
+            scores.append(-result[0]["score"])
+        elif label in neutral:
+            scores.append(0)
+        elif label in good:
+            scores.append(result[0]["score"])
+        else:
+            scores.append(0)
     return np.mean(scores)
 
 
@@ -59,9 +87,9 @@ def generate_sentiment(entries):
         chunks = chunk_text(entry["patient"])
         average_score = classify_chunks(chunks)
         label = ""
-        if average_score > 0.5:
+        if average_score > 0.1:
             label = "POSITIVE"
-        elif average_score < -0.5:
+        elif average_score < -0.1:
             label = "NEGATIVE"
         else:
             label = "NEUTRAL"
@@ -70,6 +98,8 @@ def generate_sentiment(entries):
 
 
 if __name__ == "__main__":
+    # For testing purposes
     data = load_dataset("zhengyun21/PMC-Patients")
+    data["train"] = data["train"].select(range(10))
     results = generate_sentiment(data["train"])
     print(results)
